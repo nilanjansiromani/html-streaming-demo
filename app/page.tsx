5df8cfc26ee14d844b93ba35a61486d2d4baa47f
n0s00jx <nilajan.siromani@walmart.com>
2023-05-22 10:25:08 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,12 @@ export default async function Home() {
       <h1>Product Description</h1>
       <h2>{description}</h2>
 
+      <div >
+        <img id="hero"
+          src="https://i5.walmartimages.com/asr/42919fb8-29d6-4234-92cf-315c1eea0010.fdaa0c90e47663d0455beb06bfbb348e.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
+          alt=""
+        />
+      </div>
       <h2>Comments</h2>
       <Suspense fallback={<div>Loading...</div>}>
         {/* @ts-expect-error Async Server Component */}
@@ -29,8 +35,8 @@ export default async function Home() {
       </Suspense>
 
       <div id="atc">
-          <span id="price">$75</span>
-          <span id="buynow">Add to cart</span>
+        <span id="price">$75</span>
+        <span id="buynow">Add to cart</span>
       </div>
     </>
   );
